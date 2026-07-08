606b900f63d8511041127e5eb6dabbc850a363d5
Zameer <zameermfm@gmail.com>
2026-07-08 20:18:03 +0530

    uopdt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://xadlabs.com',
   integrations: [tailwind()],
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
 });
